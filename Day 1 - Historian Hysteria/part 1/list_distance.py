731645aac8215cb856acc6242e8ac118da0594c3
SlowFlo <fbautry@gmail.com>
2024-12-09 21:52:36 +0100

    chore: unused import

--- Day 1 - Historian Hysteria/part 1/list_distance.py
+++ Day 1 - Historian Hysteria/part 1/list_distance.py
@@ -1,6 +1,3 @@
-from typing import Tuple
-
-
 def get_location_ids_total_distance(location_ids: str) -> int:
     if not isinstance(location_ids, str):
         raise TypeError("The location IDs list is not a str")
