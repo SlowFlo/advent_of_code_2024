c72c0479a90cc5acc49ecab00e2d97ac62ec1520
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-12-09 15:15:08 +0100

    feat: type verification

--- Day 1 - Historian Hysteria/part 1/list_distance.py
+++ Day 1 - Historian Hysteria/part 1/list_distance.py
@@ -2,4 +2,13 @@ def get_location_ids_total_distance(location_ids: str) -> int:
     if not isinstance(location_ids, str):
         raise TypeError("The location IDs list is not a str")
 
+    pairs = get_sorted_pairs(location_ids)
+
+    raise NotImplementedError()
+
     return 11
+
+
+def get_sorted_pairs(location_ids: str):
+    if not isinstance(location_ids, str):
+        raise TypeError("The location IDs list is not a str")
