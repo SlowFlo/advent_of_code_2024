9e67bb627221323ff8e30012cf9174e3ce858b98
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-12-09 14:21:49 +0100

    feat: test for type error

--- Day 1 - Historian Hysteria/part 1/test_list_distance.py
+++ Day 1 - Historian Hysteria/part 1/test_list_distance.py
@@ -1,3 +1,13 @@
+import pytest
+
+from list_distance import get_location_ids_total_distance
+
+
+def test_location_ids_not_a_str_must_fail():
+    with pytest.raises(TypeError):
+        get_location_ids_total_distance(45)
+
+
 def test_distance_total_is_correct():
     location_ids = """3   4
 4   3
