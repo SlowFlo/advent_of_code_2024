fc7371d2bd1524aa26cee097d790ede53326cde1
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-12-09 18:41:22 +0100

    feat: get_sorted_pairs ok

--- Day 1 - Historian Hysteria/part 1/list_distance.py
+++ Day 1 - Historian Hysteria/part 1/list_distance.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 def get_location_ids_total_distance(location_ids: str) -> int:
     if not isinstance(location_ids, str):
         raise TypeError("The location IDs list is not a str")
@@ -9,6 +12,21 @@ def get_location_ids_total_distance(location_ids: str) -> int:
     return 11
 
 
-def get_sorted_pairs(location_ids: str):
+def get_sorted_pairs(location_ids: str) -> tuple[tuple[int, int], ...]:
     if not isinstance(location_ids, str):
         raise TypeError("The location IDs list is not a str")
+
+    entries = location_ids.splitlines()
+
+    first_numbers = []
+    second_numbers = []
+
+    for entry in entries:
+        first, second = map(int, entry.split())
+        first_numbers.append(first)
+        second_numbers.append(second)
+
+    first_numbers.sort()
+    second_numbers.sort()
+
+    return tuple(zip(first_numbers, second_numbers))
