35ed6ab0aa63d5b14e94725b6e41fc0be93bd8ec
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-12-10 09:56:31 +0100

    test: get_nb_safe_reports

---
+++ Day 2 - Red-Nosed Reports/part 1/test_report_safety.py
@@ -0,0 +1,11 @@
+def test_nb_safe_reports_is_correct():
+    reports = """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"""
+
+    nb_safe_reports = get_nb_safe_reports(reports)
+
+    assert nb_safe_reports == 2
