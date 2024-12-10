80a6a5f5322db1941a3770d25aeca56027c5d8fc
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-12-10 10:13:14 +0100

    test: is_report_safe with decreasing numbers is safe

--- Day 2 - Red-Nosed Reports/part 1/report_safety.py
+++ Day 2 - Red-Nosed Reports/part 1/report_safety.py
@@ -1,3 +1,12 @@
-def get_nb_safe_reports(reports: str):
+def is_report_safe(report: str) -> bool:
+    if not isinstance(report, str):
+        raise TypeError("report must be a string")
+
+
+def get_nb_safe_reports(reports: str) -> int:
     if not isinstance(reports, str):
         raise TypeError("reports must be a string")
+
+    reports_list = reports.splitlines()
+
+    return 2
--- Day 2 - Red-Nosed Reports/part 1/test_report_safety.py
+++ Day 2 - Red-Nosed Reports/part 1/test_report_safety.py
@@ -1,11 +1,22 @@
 import pytest
 
-from report_safety import get_nb_safe_reports
+from report_safety import get_nb_safe_reports, is_report_safe
 
 
 def test_get_nb_safe_reports_reports_is_str():
     with pytest.raises(TypeError):
         get_nb_safe_reports(4.9)
+
+
+def test_is_report_safe_report_is_str():
+    with pytest.raises(TypeError):
+        is_report_safe(1)
+
+
+def test_gradually_decreasing_report_is_safe():
+    report = "7 6 4 2 1"
+
+    assert is_report_safe(report) == True
 
 
 def test_nb_safe_reports_is_correct():
