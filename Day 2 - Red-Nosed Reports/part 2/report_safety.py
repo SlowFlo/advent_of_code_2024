7b0a7d47987aaac39a7880aa40530109d3616aa3
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-12-10 19:27:50 +0100

    refactor: simplification and renoming variables

--- Day 2 - Red-Nosed Reports/part 2/report_safety.py
+++ Day 2 - Red-Nosed Reports/part 2/report_safety.py
@@ -1,32 +1,38 @@
+import time
+
+
 def is_strictly_increasing(sequence):
-    return all(sequence[i] < sequence[i + 1] for i in range(len(sequence) - 1))
+    return all(i < j for i, j in zip(sequence, sequence[1:]))
 
 
 def is_strictly_decreasing(sequence):
-    return all(sequence[i] > sequence[i + 1] for i in range(len(sequence) - 1))
+    return all(i > j for i, j in zip(sequence, sequence[1:]))
 
 
 def is_step_safe(sequence, max_step=3):
-    return all(abs(sequence[i] - sequence[i + 1]) <= max_step for i in range(len(sequence) - 1))
+    return all(abs(i - j) <= max_step for i, j in zip(sequence, sequence[1:]))
 
 
-def generate_subsequences(sequence):
-    return [sequence[:i] + sequence[i + 1 :] for i in range(len(sequence))]
+def generate_problem_dampened_reports(sequence):
+    for i in range(len(sequence)):
+        yield sequence[:i] + sequence[i + 1 :]
 
 
-def is_report_safe(report: str) -> bool:
+def is_report_safe(values):
+    return (is_strictly_increasing(values) or is_strictly_decreasing(values)) and is_step_safe(values)
+
+
+def is_problem_dampened_report_safe(report: str) -> bool:
     if not isinstance(report, str):
         raise TypeError("report must be a string")
 
     values = list(map(int, report.split()))
 
-    # Check if the report is already safe
-    if (is_strictly_increasing(values) or is_strictly_decreasing(values)) and is_step_safe(values):
+    if is_report_safe(values):
         return True
 
-    # Check whether removing one level make it safe
-    for subsequence in generate_subsequences(values):
-        if (is_strictly_increasing(subsequence) or is_strictly_decreasing(subsequence)) and is_step_safe(subsequence):
+    for problem_dampened_report in generate_problem_dampened_reports(values):
+        if is_report_safe(problem_dampened_report):
             return True
 
     return False
@@ -38,10 +44,15 @@ def get_nb_safe_reports(reports: str) -> int:
 
     reports_list = reports.splitlines()
 
-    return sum([is_report_safe(report) for report in reports_list])
+    return sum([is_problem_dampened_report_safe(report) for report in reports_list])
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     with open("puzzle_input.txt") as f:
         reports = f.read()
         print("Number of safe reports:", get_nb_safe_reports(reports))
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time:.6f} seconds")
--- Day 2 - Red-Nosed Reports/part 2/test_report_safety.py
+++ Day 2 - Red-Nosed Reports/part 2/test_report_safety.py
@@ -1,6 +1,6 @@
 import pytest
 
-from report_safety import get_nb_safe_reports, is_report_safe
+from report_safety import get_nb_safe_reports, is_problem_dampened_report_safe
 
 
 def test_get_nb_safe_reports_reports_is_str():
@@ -10,62 +10,62 @@ def test_get_nb_safe_reports_reports_is_str():
 
 def test_is_report_safe_report_is_str():
     with pytest.raises(TypeError):
-        is_report_safe(1)
+        is_problem_dampened_report_safe(1)
 
 
 def test_report_gradually_decreasing_is_safe():
     report = "7 6 4 2 1"
 
-    assert is_report_safe(report) == True
+    assert is_problem_dampened_report_safe(report) == True
 
 
 def test_report_gradually_increasing_is_safe():
     report = "1 3 6 7 9"
 
-    assert is_report_safe(report) == True
+    assert is_problem_dampened_report_safe(report) == True
 
 
 def test_report_increase_more_than_3_is_unsafe():
     report = "1 2 7 8 9"
 
-    assert is_report_safe(report) == False
+    assert is_problem_dampened_report_safe(report) == False
 
 
 def test_report_decrease_more_than_3_is_unsafe():
     report = "9 7 6 2 1"
 
-    assert is_report_safe(report) == False
+    assert is_problem_dampened_report_safe(report) == False
 
 
 def test_both_increasing_and_decreasing_is_unsafe():
     report = "1 3 2 4 2"
 
-    assert is_report_safe(report) == False
+    assert is_problem_dampened_report_safe(report) == False
 
 
 def test_report_become_safe_by_removing_a_decreasing_level():
     # become safe if 3 is removed
     report = "1 3 2 4 5"
 
-    assert is_report_safe(report) == True
+    assert is_problem_dampened_report_safe(report) == True
 
 
 def test_report_become_safe_by_removing_the_first_level():
     report = "5 1 2 3 4 5"
 
-    assert is_report_safe(report) == True
+    assert is_problem_dampened_report_safe(report) == True
 
 
 def test_neither_increasing_or_decreasing_is_unsafe():
     report = "8 6 4 4 4 1"
 
-    assert is_report_safe(report) == False
+    assert is_problem_dampened_report_safe(report) == False
 
 
 def test_report_become_safe_by_removing_a_level_of_same_value():
     report = "8 6 4 4 1"
 
-    assert is_report_safe(report) == True
+    assert is_problem_dampened_report_safe(report) == True
 
 
 def test_nb_safe_reports_is_correct():
