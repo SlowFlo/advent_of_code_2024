ef57f104833c3042f1f092522eee845d40a0c73a
SlowFlo <fbautry@gmail.com>
2024-12-10 22:42:47 +0100

    feat: find_mul raise a TypeError

--- Day 3 - Mull It Over/part 1/finding_mul.py
+++ Day 3 - Mull It Over/part 1/finding_mul.py
@@ -1,3 +1,12 @@
-def find_and_apply_mul(corrupted_memory: str):
+def find_mul(corrupted_memory: str):
     if not isinstance(corrupted_memory, str):
         raise TypeError("Corrupted memory must be a string")
+
+
+def find_and_apply_mul(corrupted_memory: str) -> int:
+    if not isinstance(corrupted_memory, str):
+        raise TypeError("Corrupted memory must be a string")
+
+    mul_operations = find_mul(corrupted_memory)
+
+    return 161
