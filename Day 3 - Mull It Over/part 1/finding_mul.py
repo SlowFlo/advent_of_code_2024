df3e10518eb81a6fb852ae0b33df347cc133c93a
SlowFlo <fbautry@gmail.com>
2024-12-10 23:05:40 +0100

    feat: find_mul("mul(0,0)") return (0,0),)

--- Day 3 - Mull It Over/part 1/finding_mul.py
+++ Day 3 - Mull It Over/part 1/finding_mul.py
@@ -2,6 +2,10 @@ def find_mul(corrupted_memory: str):
     if not isinstance(corrupted_memory, str):
         raise TypeError("Corrupted memory must be a string")
 
+    if corrupted_memory.startswith("mul(") and corrupted_memory.endswith(")"):
+        x, y = map(int, corrupted_memory.strip("mul()").split(","))
+        return ((x, y),)
+
     return ()
 
 
