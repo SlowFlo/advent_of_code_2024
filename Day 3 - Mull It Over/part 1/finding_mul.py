3f6d5b70919917231f777330562bc45aa2d9bc69
SlowFlo <fbautry@gmail.com>
2024-12-10 22:47:08 +0100

    feat: find_mul() empty str return empty tuple

--- Day 3 - Mull It Over/part 1/finding_mul.py
+++ Day 3 - Mull It Over/part 1/finding_mul.py
@@ -2,6 +2,8 @@ def find_mul(corrupted_memory: str):
     if not isinstance(corrupted_memory, str):
         raise TypeError("Corrupted memory must be a string")
 
+    return ()
+
 
 def find_and_apply_mul(corrupted_memory: str) -> int:
     if not isinstance(corrupted_memory, str):
