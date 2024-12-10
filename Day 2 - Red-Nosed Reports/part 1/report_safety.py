6af6fd3160afd31b0830d1a5a9138080971ef655
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-12-10 10:55:05 +0100

    feat: unsafe when both decreasing and increasing

--- Day 2 - Red-Nosed Reports/part 1/report_safety.py
+++ Day 2 - Red-Nosed Reports/part 1/report_safety.py
@@ -4,10 +4,15 @@ def is_report_safe(report: str) -> bool:
 
     levels = list(map(int, report.split()))
     previous_level = levels[0]
+    previous_is_increasing = None
     for level in levels[1:]:
+        is_increasing = True if level - previous_level > 0 else False
+        if previous_is_increasing and previous_is_increasing != is_increasing:
+            return False
         if abs(level - previous_level) > 3:
             return False
         previous_level = level
+        previous_is_increasing = is_increasing
 
     return True
 
