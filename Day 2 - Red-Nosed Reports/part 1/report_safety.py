50c2181ec76780f65f6fbfbac3ded967238c8d9c
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-12-10 11:07:25 +0100

    refactor: simplifications

--- Day 2 - Red-Nosed Reports/part 1/report_safety.py
+++ Day 2 - Red-Nosed Reports/part 1/report_safety.py
@@ -5,9 +5,13 @@ def is_report_safe(report: str) -> bool:
     levels = list(map(int, report.split()))
     previous_level = levels[0]
     previous_is_increasing = None
+
     for level in levels[1:]:
-        is_increasing = True if level - previous_level > 0 else False
-        if previous_is_increasing and previous_is_increasing != is_increasing:
+        is_increasing = level > previous_level
+        if (
+            previous_is_increasing is not None
+            and previous_is_increasing != is_increasing
+        ):
             return False
         difference_previous_level = abs(level - previous_level)
         if difference_previous_level == 0 or difference_previous_level > 3:
