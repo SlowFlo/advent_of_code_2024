809a5bfbd73bcb2b45dc60e05829dcac1e3f3b70
SlowFlo <fbautry@gmail.com>
2024-12-10 22:36:46 +0100

    test: find_and_apply_mul accept a str

--- Day 3 - Mull It Over/part 1/test_finding_mul.py
+++ Day 3 - Mull It Over/part 1/test_finding_mul.py
@@ -1,6 +1,20 @@
-def test_find_and_apply_mul_is_correct():
-    reports = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+import pytest
 
-    total = find_and_apply_mul(reports)
+from finding_mul import find_and_apply_mul
+
+
+def test_find_and_apply_mul_corrupted_memory_is_str():
+    with pytest.raises(TypeError):
+        find_and_apply_mul(None)
+
+
+def test_find_and_apply_mul_is_correct():
+    corrupted_memory = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+
+    total = find_and_apply_mul(corrupted_memory)
 
     assert total == 161
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
