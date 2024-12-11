4349a0563d942c6d58bb7ae6668e40e33bb30853
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-12-11 18:52:39 +0100

    feat: multiple operations

--- Day 3 - Mull It Over/part 1/finding_mul.py
+++ Day 3 - Mull It Over/part 1/finding_mul.py
@@ -6,12 +6,26 @@ def find_all_mul(corrupted_memory: str) -> tuple[tuple[int, int], ...]:
     if not isinstance(corrupted_memory, str):
         raise TypeError("Corrupted memory must be a string")
 
-    if corrupted_memory.startswith("mul(") and corrupted_memory.endswith(")"):
-        x_str, y_str = corrupted_memory.strip("mul()").split(",")
-        if is_mul_inputs_valid(x_str, y_str):
-            return ((int(x_str), int(y_str)),)
+    mul_operation_start_idx = corrupted_memory.find("mul(")
+    if mul_operation_start_idx == -1:
+        mul_operation_end_idx = -1
+    else:
+        mul_operation_end_idx = corrupted_memory.find(")", mul_operation_start_idx)
 
-    return ()
+    mul_operations = []
+
+    while mul_operation_end_idx != -1:
+        x_str, y_str = corrupted_memory[mul_operation_start_idx:mul_operation_end_idx].strip("mul()").split(",")
+        if is_mul_inputs_valid(x_str, y_str):
+            mul_operations.append((int(x_str), int(y_str)))
+
+        mul_operation_start_idx = corrupted_memory.find("mul(", mul_operation_end_idx)
+        if mul_operation_start_idx == -1:
+            mul_operation_end_idx = -1
+        else:
+            mul_operation_end_idx = corrupted_memory.find(")", mul_operation_start_idx)
+
+    return tuple(mul_operations)
 
 
 def find_and_apply_mul(corrupted_memory: str) -> int:
