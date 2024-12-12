6db666ec61ad2c26460a7730435caa447a8470b5
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-12-12 19:15:29 +0100

    refactor: using regex to simplify greatly the code

--- Day 3 - Mull It Over/part 1/finding_mul.py
+++ Day 3 - Mull It Over/part 1/finding_mul.py
@@ -1,38 +1,20 @@
-from typing import Tuple
-
-
-def is_mul_inputs_valid(x: str, y: str) -> bool:
-    return len(x) <= 3 and len(y) <= 3 and x.isnumeric() and y.isnumeric()
-
-
-def get_next_mul_idx(corrupted_memory: str, start_idx: int = 0) -> tuple[int, int]:
-    mul_start_idx = corrupted_memory.find("mul(", start_idx)
-    if mul_start_idx == -1:
-        mul_end_idx = -1
-    else:
-        next_mul_operation = corrupted_memory.find("mul(", mul_start_idx + 1)
-        mul_end_idx = corrupted_memory.find(")", mul_start_idx)
-        if next_mul_operation != -1 and next_mul_operation < mul_end_idx:
-            mul_start_idx = next_mul_operation
-    return mul_start_idx, mul_end_idx
+import re
 
 
 def find_all_mul(corrupted_memory: str) -> tuple[tuple[int, int], ...]:
     if not isinstance(corrupted_memory, str):
-        raise TypeError("Corrupted memory must be a string")
+        raise TypeError("Corrupted_memory must be a string")
 
-    mul_start_idx, mul_end_idx = get_next_mul_idx(corrupted_memory)
+    if not corrupted_memory.strip():
+        return ()
 
-    mul_operations = []
+    valid_mul_pattern = r"mul\([0-9]{1,3},[0-9]{1,3}\)"
 
-    while mul_end_idx != -1:
-        x_str, y_str = corrupted_memory[mul_start_idx:mul_end_idx].strip("mul()").split(",")
-        if is_mul_inputs_valid(x_str, y_str):
-            mul_operations.append((int(x_str), int(y_str)))
+    matches = re.findall(valid_mul_pattern, corrupted_memory)
 
-        mul_start_idx, mul_end_idx = get_next_mul_idx(corrupted_memory, mul_end_idx)
+    result = [tuple(map(int, match[4:-1].split(","))) for match in matches]
 
-    return tuple(mul_operations)
+    return tuple(result)
 
 
 def find_and_apply_mul(corrupted_memory: str) -> int:
