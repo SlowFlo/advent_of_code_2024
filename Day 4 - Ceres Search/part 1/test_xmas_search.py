aed4c1a61ca0d2f28774c01f218e7aa138e6825c
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-12-14 16:50:55 +0100

    test: one line with no xmas

--- Day 4 - Ceres Search/part 1/test_xmas_search.py
+++ Day 4 - Ceres Search/part 1/test_xmas_search.py
@@ -12,7 +12,11 @@ def test_empty_str_no_xmas():
     assert find_number_of_xmas("") == 0
 
 
-def test_one_line_with_xmas():
+def test_1_line_no_xmas():
+    assert find_number_of_xmas("AZERTY") == 0
+
+
+def test_1_line_with_xmas():
     assert find_number_of_xmas("XMAS") == 1
 
 
