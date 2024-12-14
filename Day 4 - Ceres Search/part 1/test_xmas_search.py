2662d010492a20d8709851f3f08013a098e8c5ec
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-12-14 16:30:00 +0100

    test: find_number_of_xmas use case

--- Day 4 - Ceres Search/part 1/test_xmas_search.py
+++ Day 4 - Ceres Search/part 1/test_xmas_search.py
@@ -8,5 +8,20 @@ def test_find_number_of_xmas_input_grid_is_str():
         find_number_of_xmas(45)
 
 
+def test_find_number_of_xmas_use_case_is_ok():
+    letters_grid = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"""
+
+    assert find_number_of_xmas(letters_grid) == 18
+
+
 if __name__ == "__main__":
     pytest.main([__file__])
