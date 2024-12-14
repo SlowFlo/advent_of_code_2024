b44986da8ae0ef2d78d4b1281536bbd1d8ec3111
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-12-14 18:22:02 +0100

    test: lines to diagonals

--- Day 4 - Ceres Search/part 1/xmas_search.py
+++ Day 4 - Ceres Search/part 1/xmas_search.py
@@ -8,6 +8,21 @@ def lines_to_columns(texte: str) -> str:
     return "\n".join("".join(colonne) for colonne in colonnes)
 
 
+def lines_to_diagonals(texte: str) -> str:
+    lines = texte.splitlines()
+    height = len(lines)
+    width = len(lines[0])
+
+    diagonals = [[] for _ in range(height + width - 1)]
+
+    for i, line in enumerate(lines):
+        for j, char in enumerate(line):
+            diagonals[i + j].append(char)
+
+    result = "\n".join("".join(reversed(diagonal)) for diagonal in diagonals)
+    return result
+
+
 def find_number_of_xmas(letters_grid: str) -> int:
     if not isinstance(letters_grid, str):
         raise TypeError("letters_grid must be a string")
@@ -16,12 +31,15 @@ def find_number_of_xmas(letters_grid: str) -> int:
         return 0
 
     reversed_grid = lines_to_columns(letters_grid)
+    diagonals = lines_to_diagonals(letters_grid)
 
     return (
         letters_grid.count("XMAS")
         + letters_grid[::-1].count("XMAS")
+        + diagonals.count("XMAS")
         + reversed_grid.count("XMAS")
         + reversed_grid[::-1].count("XMAS")
+        + diagonals[::-1].count("XMAS")
     )
 
 
