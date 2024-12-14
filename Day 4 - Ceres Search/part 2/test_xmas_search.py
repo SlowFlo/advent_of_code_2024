e083d298b1bc3d8111adfe5c26d19cac8b356b9c
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-12-14 22:29:24 +0100

    test: adapting the tests to the new problem

--- Day 4 - Ceres Search/part 2/test_xmas_search.py
+++ Day 4 - Ceres Search/part 2/test_xmas_search.py
@@ -1,118 +1,30 @@
 import pytest
 
-from xmas_search import find_number_of_xmas
+from xmas_search import find_number_of_x_mas
 
 
-def test_find_number_of_xmas_input_grid_is_str():
+def test_find_number_of_x_mas_input_grid_is_str():
     with pytest.raises(TypeError):
-        find_number_of_xmas(45)
+        find_number_of_x_mas(45)
 
 
-def test_empty_str_no_xmas():
-    assert find_number_of_xmas("") == 0
+def test_empty_str_no_x_mas():
+    assert find_number_of_x_mas("") == 0
 
 
-def test_1_line_no_xmas():
-    assert find_number_of_xmas("AZERTY") == 0
+def test_1_line_no_x_mas():
+    assert find_number_of_x_mas("AZERTY") == 0
 
 
-def test_1_line_with_1_xmas():
-    assert find_number_of_xmas("XMAS") == 1
+def test_1_x_mas():
+    column = """M.S
+.A.
+M.S"""
+
+    assert find_number_of_x_mas(column) == 1
 
 
-def test_1_line_with_2_xmas():
-    assert find_number_of_xmas("ZQDQZXMASerzQDZXMASZQD") == 2
-
-
-def test_1_line_with_1_reversed_xmas():
-    assert find_number_of_xmas("SAMX") == 1
-
-
-def test_1_line_with_2_reversed_xmas():
-    assert find_number_of_xmas("DZSAMX45GUVSAMXTCTC") == 2
-
-
-def test_1_line_with_both_normal_and_reversed_xmas():
-    assert find_number_of_xmas("DZXMAS45GUVSAMXTCTC") == 2
-
-
-def test_1_column_with_1_xmas():
-    column = """X
-M
-A
-S"""
-
-    assert find_number_of_xmas(column) == 1
-
-
-def test_1_column_with_2_xmas():
-    column = """2
-X
-M
-A
-S
-Z
-D
-X
-M
-A
-S
-d
-T
-"""
-
-    assert find_number_of_xmas(column) == 2
-
-
-def test_1_column_with_1_reversed_xmas():
-    column = """S
-A
-M
-X"""
-
-    assert find_number_of_xmas(column) == 1
-
-
-def test_1_column_with_2_reversed_xmas():
-    column = """E
-R
-S
-A
-M
-X
-S
-A
-M
-X"""
-
-    assert find_number_of_xmas(column) == 2
-
-
-def test_1_column_with_both_normal_and_reversed_xmas():
-    column = """E
-    
-X
-M
-A
-S
-S
-A
-M
-X"""
-
-    assert find_number_of_xmas(column) == 2
-
-
-def test_1_diagonal_with_1_xmas():
-    column = """X...
-.M..
-..A.
-...S"""
-
-    assert find_number_of_xmas(column) == 1
-
-
-def test_find_number_of_xmas_use_case_is_ok():
+def test_find_number_of_x_mas_use_case_is_ok():
     letters_grid = """MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
@@ -124,7 +36,7 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX"""
 
-    assert find_number_of_xmas(letters_grid) == 18
+    assert find_number_of_x_mas(letters_grid) == 9
 
 
 if __name__ == "__main__":
