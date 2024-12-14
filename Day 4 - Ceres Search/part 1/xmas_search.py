4d53715378e0439cd0c4e9b0bfbc3ddd3ade723a
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2024-12-14 19:12:13 +0100

    fix: doing also the diagonal from left to right

--- Day 4 - Ceres Search/part 1/xmas_search.py
+++ Day 4 - Ceres Search/part 1/xmas_search.py
@@ -1,15 +1,15 @@
 import time
 
 
-def lines_to_columns(texte: str) -> str:
-    lignes = texte.splitlines()
+def lines_to_columns(text: str) -> str:
+    lignes = text.splitlines()
     # We group each Nth letter of each line together
     colonnes = zip(*lignes)
     return "\n".join("".join(colonne) for colonne in colonnes)
 
 
-def lines_to_diagonals(texte: str) -> str:
-    lines = texte.splitlines()
+def lines_to_diagonals(text: str) -> str:
+    lines = text.splitlines()
     height = len(lines)
     width = len(lines[0])
 
@@ -32,14 +32,18 @@ def find_number_of_xmas(letters_grid: str) -> int:
 
     reversed_grid = lines_to_columns(letters_grid)
     diagonals = lines_to_diagonals(letters_grid)
+    reversed_lines = "\n".join([line[::-1] for line in letters_grid.splitlines()])
+    reversed_diagonals = lines_to_diagonals(reversed_lines)
 
     return (
         letters_grid.count("XMAS")
         + letters_grid[::-1].count("XMAS")
         + diagonals.count("XMAS")
+        + diagonals[::-1].count("XMAS")
         + reversed_grid.count("XMAS")
         + reversed_grid[::-1].count("XMAS")
-        + diagonals[::-1].count("XMAS")
+        + reversed_diagonals.count("XMAS")
+        + reversed_diagonals[::-1].count("XMAS")
     )
 
 
