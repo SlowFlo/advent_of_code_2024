07a72cb7a7f0160e4b9c3efd82bcbe8e1a4e4b6a
SlowFlo <fbautry@gmail.com>
2024-12-19 00:13:12 +0100

    test: correct dict when 2 time the same first number in rules

--- Day 5 - Print Queue/part 1/test_page_ordering.py
+++ Day 5 - Print Queue/part 1/test_page_ordering.py
@@ -47,6 +47,13 @@ def test_get_dict_ordering_rules_of_2_correct_rules_different_first_number_retur
     assert get_dict_ordering_rules(rules) == {47: (53,), 97: (13,)}
 
 
+def test_get_dict_ordering_rules_of_2_correct_rules_same_first_number_return_correct_dict():
+    rules = """47|53
+47|56"""
+
+    assert get_dict_ordering_rules(rules) == {47: (53, 56)}
+
+
 def test_use_case():
     rules_and_updates = """47|53
 97|13
